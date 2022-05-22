fb9fcea38fc59e5fa5ff189ae249c82fdad411ae
nischal-bhatt <nischal.bhatt.87@gmail.com>
2022-05-22 15:52:57 +0800

    committing

--- src/main/java/com/in28minutes/jpa/hibernate/entity/Course.java
+++ src/main/java/com/in28minutes/jpa/hibernate/entity/Course.java
@@ -6,9 +6,9 @@ import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
@@ -32,6 +32,11 @@ public class Course {
 	@OneToMany(mappedBy="course" /*fetch=FetchType.EAGER*/)
 	private List<Review> reviews = new ArrayList<>();
 
+	@ManyToMany(mappedBy="courses")
+	//student is the owning side
+	private List<Student> students = new ArrayList<>();
+	
+	
 	@UpdateTimestamp
 	private LocalDateTime lastUpdatedDate;
 	@CreationTimestamp
@@ -68,6 +73,16 @@ public class Course {
 	
 	public void removeReview(Review review) {
 		this.reviews.remove(review);
+	}
+
+	
+	
+	public List<Student> getStudents() {
+		return students;
+	}
+
+	public void addStudents(Student student) {
+		this.students.add(student);
 	}
 
 	@Override
--- src/main/java/com/in28minutes/jpa/hibernate/entity/Student.java
+++ src/main/java/com/in28minutes/jpa/hibernate/entity/Student.java
@@ -1,11 +1,16 @@
 package com.in28minutes.jpa.hibernate.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -18,9 +23,15 @@ public class Student {
 	@Column(nullable = false)
 	private String name;
 
-	@OneToOne(fetch=FetchType.LAZY) //default is eager
-	@JoinColumn(name="theRockJoiningColumn")
+	@OneToOne(fetch = FetchType.LAZY) // default is eager
+	@JoinColumn(name = "theRockJoiningColumn")
 	private Passport passport;
+
+	@ManyToMany
+	@JoinTable(name = "STUDENT_COURSE", 
+	joinColumns = @JoinColumn(name = "Student_id"), 
+	inverseJoinColumns = @JoinColumn(name = "course_id"))
+	private List<Course> courses = new ArrayList<>();
 
 	public Student() {
 		// need to have a no args constructor for jpa
@@ -49,6 +60,14 @@ public class Student {
 
 	public Long getId() {
 		return id;
+	}
+
+	public List<Course> getCourses() {
+		return courses;
+	}
+
+	public void addCourse(Course course) {
+		this.courses.add(course);
 	}
 
 	@Override
